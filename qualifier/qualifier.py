33c00a2da6e77d39f7fe36ac521096e6c00124e4
Dhananjay <dhananjay.latkar@gmail.com>
2022-06-29 23:57:09 +0530

    submission

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -20,6 +20,8 @@ class RestaurantManager:
         already defined a staff dictionary.
         """
         self.staff = {}
+        self.special = {}
+        self.busy = set()
 
     async def __call__(self, request: Request):
         """Handle a request received.
@@ -31,4 +33,43 @@ class RestaurantManager:
             Request object containing information about the sent
             request to your application.
         """
-        ...
+        _scope = request.scope
+        match _scope["type"]:
+            case "staff.onduty":
+                self.staff[_scope["id"]] = request
+                for spe in _scope["speciality"]:
+                    _special = self.special.get(spe, set())
+                    _special.add(_scope["id"])
+                    self.special[spe] = _special
+                    # print(spe, "$$$", self.special[spe])
+
+            case "staff.offduty":
+                for spe, sta in self.special.items():
+                    if _scope["id"] in sta:
+                        sta.remove(_scope["id"])
+                self.staff.pop(_scope["id"])
+
+            case "order":
+                found = None
+                if _scope["speciality"] in self.special:
+                    for sta in self.special[_scope["speciality"]]:
+                        # print("Found speciality")
+                        if sta not in self.busy:
+                            # print("Using speciality")
+                            found = self.staff[sta]
+                            break
+
+                if not found:
+                    for sta in self.staff.keys():
+                        if sta not in self.busy:
+                            found = self.staff[sta]
+                            break
+
+                self.busy.add(sta)
+                full_order = await request.receive()
+                await found.send(full_order)
+
+                result = await found.receive()
+                await request.send(result)
+
+                self.busy.remove(sta)
